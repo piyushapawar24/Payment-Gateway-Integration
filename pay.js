6f162b716d3eb8da5ac757a81d8d83222ad53c9c
Piyusha Pawar <86230045+piyushapawar24@users.noreply.github.com>
2022-01-19 19:14:06 +0500

    Add files via upload

---
+++ pay.js
@@ -0,0 +1,9 @@
+function myfun() {
+    document.getElementById("myform").reset();
+    alert("Payment Successfull....thank you :)")
+}
+
+function pay() {
+  
+    alert("Payment Successfull....thank you :)")
+}
